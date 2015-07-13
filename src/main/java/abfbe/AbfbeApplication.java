3ad4e5c503c77c8060a04d072854989109a3d987
mauret <maurodeluca83@gmail.com>
2015-07-14 00:04:45 +0200

    First commit

---
+++ src/main/java/abfbe/AbfbeApplication.java
@@ -0,0 +1,12 @@
+package abfbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbfbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbfbeApplication.class, args);
+    }
+}
