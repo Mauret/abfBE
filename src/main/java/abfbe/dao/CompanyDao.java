872664c92536a13212b2166da15d9e91a28b024d
mauret <maurodeluca83@gmail.com>
2015-08-04 12:59:18 +0200

    AF-23 Visualizzazione elenco delle aziende partecipanti

--- src/main/java/abfbe/dao/CompanyDao.java
+++ src/main/java/abfbe/dao/CompanyDao.java
@@ -3,6 +3,7 @@ package abfbe.dao;
 import abfbe.entities.Company;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by mauret on 02/08/15.
@@ -10,5 +11,6 @@ import org.springframework.stereotype.Repository;
  * Class for accessing to the company table in the database.
  */
 @Repository
+@Transactional
 public interface CompanyDao extends CrudRepository<Company, Long> {
 }
