8e2a1befdc95368549bfce506e6b36f2a6057eb6
mauret <maurodeluca83@gmail.com>
2015-08-14 06:27:41 +0200

    AF-23 Visualizzazione elenco delle aziende partecipanti

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
-INSERT INTO af.company (name, address, zipcode, city, phone, email)
-VALUES ('azienda1', 'via delle prove 1', '12345', 'Città delle prove', '1234567890', 'mail@example.com');
+INSERT INTO af.company (name, address, zipcode, city, phone, email, description)
+VALUES ('azienda1', 'via delle prove 1', '12345', 'Città delle prove', '1234567890', 'mail@example.com', 'descrizione azienda1');
 
-INSERT INTO af.company (name, address, zipcode, city, phone, email)
-VALUES ('azienda2', 'via delle prove 2', '12345', 'Città delle prove', '1234567890', 'mail@example.com');
+INSERT INTO af.company (name, address, zipcode, city, phone, email, description)
+VALUES ('azienda2', 'via delle prove 2', '12345', 'Città delle prove', '1234567890', 'mail@example.com', 'descrizione azienda2');
 
-INSERT INTO af.company (name, address, zipcode, city, phone, email)
-VALUES ('azienda3', 'via delle prove 3', '12345', 'Città delle prove', '1234567890', 'mail@example.com');
+INSERT INTO af.company (name, address, zipcode, city, phone, email, description)
+VALUES ('azienda3', 'via delle prove 3', '12345', 'Città delle prove', '1234567890', 'mail@example.com', 'descrizione azienda3');
 
-INSERT INTO af.company (name, address, zipcode, city, phone, email)
-VALUES ('azienda4', 'via delle prove 4', '12345', 'Città delle prove', '1234567890', 'mail@example.com');
+INSERT INTO af.company (name, address, zipcode, city, phone, email, description)
+VALUES ('azienda4', 'via delle prove 4', '12345', 'Città delle prove', '1234567890', 'mail@example.com', 'descrizione azienda4');
 
-INSERT INTO af.company (name, address, zipcode, city, phone, email)
-VALUES ('azienda5', 'via delle prove 5', '12345', 'Città delle prove', '1234567890', 'mail@example.com');
+INSERT INTO af.company (name, address, zipcode, city, phone, email, description)
+VALUES ('azienda5', 'via delle prove 5', '12345', 'Città delle prove', '1234567890', 'mail@example.com', 'descrizione azienda5');
